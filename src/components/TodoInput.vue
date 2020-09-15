468a5696ccda8f674b34a10e416c10862e166c44
Alice1017 <genocidedragon@gmail.com>
2020-09-15 13:36:08 +0900

    FEAT: TodoInput component

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <input
+    class="new-todo"
+    autocomplete="off"
+    placeholder="What needs to be done?"
+    v-model="todo"
+    @keydown.enter="emitEvent(todo)"
+    autofocus
+  >
+</template>
+
+<script setup="props, { emit }">
+import { ref } from "vue";
+
+export default {
+  name: "TodoInput",
+}
+
+
+
+export const todo = ref("");
+export const emitEvent = (newone) => {
+  emit("add", newone);
+  todo.value = "";
+};
+</script>
